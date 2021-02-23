fb4250f6c572f6bc6a5deb279b83f9255cc87d7c
an2onsol <74258164+an2onsol@users.noreply.github.com>
2021-02-23 22:44:47 +0300

    1.2(25)

---
+++ 1.25/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void palindrome(int x) {
+	int i = 0, dec = x, flag = 1;
+	int* bin = (int*) malloc(((log(x) / log(2)) + 1) * sizeof(int));
+
+	while (x >= 1) {
+		bin[i] = x % 2;
+		x /= 2;
+		i++;
+	}
+	int j;
+	for (j = 0; j <= i / 2; j++) {
+		if (bin[j] != bin[i - 1 - j]) {
+			flag = 0;
+			break;
+		}
+	}
+	if (flag) {
+		for (j = 0; j < i; j++) {
+			printf("%d", bin[j]);
+		}
+		printf("--%d \n", dec);
+	}
+	free(bin);
+}
+
+int main() {
+	int m, i;
+	scanf("%d", &m);
+	if (m >= 1) {
+		printf("1--1 \n");
+		for (i = 3; i <= m; i += 2) {
+			palindrome(i);
+		}
+	}
+	return 0;
+}
