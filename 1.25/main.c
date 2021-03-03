3acedc6cf5e70e865a5e2dd27a00d43c9cd66b7a
an2onsol <74258164+an2onsol@users.noreply.github.com>
2021-03-03 09:32:36 +0300

    final fix

--- 1.25/main.c
+++ 1.25/main.c
@@ -5,7 +5,9 @@
 void palindrome(int x) {
 	int i = 0, dec = x, flag = 1;
 	int* bin = (int*) malloc(((int)(log(x) / log(2)) + 1) * sizeof(int));
-
+	if(bin == NULL){
+		exit(2);
+	}
 	while (x >= 1) {
 		bin[i] = x % 2;
 		x /= 2;
@@ -28,10 +30,14 @@ void palindrome(int x) {
 }
 
 int main() {
-	int m, i;
-	scanf("%d", &m);
-	if (m < 1) {
-		exit(1);
+	int m = 0, i, flag = 0;
+	while(flag != 1 || m < 1){
+		flag = scanf("%d", &m);
+		if(flag != 1 || m < 1){
+			while(getchar() != '\n');
+			printf("Incorrect \n");
+		}
+		
 	}
 	printf("1--1 \n");
 	for (i = 3; i <= m; i += 2) {
